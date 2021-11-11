a58b5ef067ca527f0b0a37025573b3c2b3d1ebf3
Hannah Y <hannah@corgibyte.com>
2021-11-11 14:27:34 -0800

    Add UI functionality for time left

--- src/index.js
+++ src/index.js
@@ -8,14 +8,28 @@ $('#inputForm').on('submit', function(event) {
   event.preventDefault();
   const submittedAge = new Age(new Date($("#birthday").val()));
   outputAges(submittedAge);
+  outputLifeExp(submittedAge, parseFloat($("#lifeExp").val()));
 });
 
 function outputAges(age) {
-  $("#outputs").show();
+  $(".outputs").show();
   $("#earthAge").text(age.earthAge.toPrecision(3) + " Earth solar years");
   $("#mercurialAge").text(age.toMercurial().toPrecision(3) + " Mercurial solar years");
   $("#venusianAge").text(age.toVenusian().toPrecision(3) + " Venusian solar years");
   $("#martianAge").text(age.toMartian().toPrecision(3) + " Martian solar years");
   $("#jovianAge").text(age.toJovian().toPrecision(3) + " Jovian solar years");
   $("#dateOutput").text(age.birthday.toDateString());
+}
+
+function outputLifeExp(age, estLifeExp) {
+  if (age.earthAge > estLifeExp) {
+    $("#lifeExpTitle").text("Estimated time left:");
+  } else {
+    $("#lifeExpTitle").text("Time past estimated:");
+  }
+  $("#earthLifeExp").text(age.earthYearsLeft(estLifeExp).toPrecision(3) + " Earth solar years");
+  $("#mercurialLifeExp").text(age.mercurialYearsLeft(estLifeExp).toPrecision(3) + " Mercurial solar years");
+  $("#venusianLifeExp").text(age.venusianYearsLeft(estLifeExp).toPrecision(3) + " Venusian solar years");
+  $("#martianLifeExp").text(age.martianYearsLeft(estLifeExp).toPrecision(3) + " Martian solar years");
+  $("#jovianLifeExp").text(age.jovianYearsLeft(estLifeExp).toPrecision(3) + " Jovian solar years");
 }
