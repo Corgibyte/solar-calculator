e1a653c329609b4bbfe131d7ccf9bcb0ccad5ef7
Hannah Y <hannah@corgibyte.com>
2021-11-11 09:07:30 -0800

    Add return to spec methods to allow good fails

--- src/age.js
+++ src/age.js
@@ -5,17 +5,21 @@ export class Age {
 
   toMercurial() {
     //TODO
+    return 1;
   }
 
   toVenusian() {
     //TODO
+    return 1;
   }
 
   toMartian() {
     //TODO
+    return 1;
   }
 
   toJovian() {
     //TODO
+    return 1;
   }
 }
